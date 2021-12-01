78813d6fa732a541cc2a1c4d29db66a23b64d13e
Ethan Hsu <ethanhsuhsu@gmail.com>
2021-11-30 22:44:35 -0800

    Day 1: Sonar Sweep

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map(String::toInt).zipWithNext { a, b -> a < b }.count { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map(String::toInt).windowed(3) { it.sum() }.zipWithNext { a, b -> a < b }.count { it }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
